ba09f2beb79abc5b64bf6a27c633136a8605991b
rick-ssa <rick_ssa@hotmail.com>
2020-05-02 09:46:40 -0300

    created categories table

--- backend/src/database/tables.js
+++ backend/src/database/tables.js
@@ -11,8 +11,24 @@ async function createTablesUsers() {
     sql += "name VARCHAR(200) NOT NULL, "
     sql += "email VARCHAR(100) NOT NULL, "
     sql += "cnpj VARCHAR(50) NOT NULL, "
-    sql += "wathsapp VARCHAR(50) NOT NULL, "
+    sql += "wathsapp VARCHAR(50) , "
     sql += "type VARCHAR(20) NOT NULL"
+    sql += ")"
+
+    con.query(sql, (err)=>{
+        if(err) throw err
+
+        con.end()
+    })
+}
+
+async function createTablesProducts() {
+    const con = await connection()
+
+    let sql = ''
+    sql = "CREATE TABLE IF NOT EXISTS vtex_categories("
+    sql += "id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, "
+    sql += "name VARCHAR(200) NOT NULL"
     sql += ")"
 
     con.query(sql, (err)=>{
@@ -24,6 +40,7 @@ async function createTablesUsers() {
 
 async function createTables() {
     await createTablesUsers()
+    await createTablesProducts()
 
 }
 
